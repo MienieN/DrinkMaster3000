ef1f4b983820289cc50003f73a013356f9c02495
Porslin97 <mimmi.nord97@gmail.com>
2024-03-23 13:01:24 +0100

    made the text on the buttons dynamically update

--- src/GUI/Boundary/MainPanel.java
+++ src/GUI/Boundary/MainPanel.java
@@ -1,43 +1,55 @@
 package src.GUI.Boundary;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class MainPanel extends JPanel {
     private MainFrame mainFrame;
-    private JButton ingredient1Button = new JButton();
-    private JButton ingredient2Button = new JButton();
-    private JButton ingredient3Button = new JButton();
-    private JButton ingredient4Button = new JButton();
+    private JButton[] ingredientButtons = new JButton[4];
+
+    //TODO look into a prettier way to do action listener
+    private ActionListener ingredientButtonPressed = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            displayNextButtons((JButton)e.getSource());
+        }
+    };
     private int counter = 0;
     private ArrayList<String> ingredientNames;
     public MainPanel(MainFrame mainFrame) {
         this.mainFrame = mainFrame;
         setSize(mainFrame.getWidth(), mainFrame.getHeight());
         setLayout(null);
-        ingredient1Button.setSize(100, 100);
-        ingredient2Button.setSize(100, 100);
-        ingredient3Button.setSize(100, 100);
-        ingredient4Button.setSize(100, 100);
-        ingredient1Button.setLocation(100, 100);
-        ingredient2Button.setLocation(200, 100);
-        ingredient3Button.setLocation(100, 200);
-        ingredient4Button.setLocation(200, 200);
-        add(ingredient1Button);
-        add(ingredient2Button);
-        add(ingredient3Button);
-        add(ingredient4Button);
+        for (int i = 0; i < ingredientButtons.length; i++) {
+            ingredientButtons[i] = new JButton();
+            ingredientButtons[i].setSize(100, 100);
+            ingredientButtons[i].setName(String.valueOf(i));
+            ingredientButtons[i].addActionListener(ingredientButtonPressed);
+        }
 
+        //TODO make a better way to set location of buttons
+        ingredientButtons[0].setLocation(100, 100);
+        ingredientButtons[1].setLocation(200, 100);
+        ingredientButtons[2].setLocation(100, 200);
+        ingredientButtons[3].setLocation(200, 200);
+        for (int i = 0; i < ingredientButtons.length; i++) {
+            add(ingredientButtons[i]);
+        }
     }
 
     public void receiveIngredientsList(ArrayList<String> ingredientNames) {
         this.ingredientNames = ingredientNames;
-        displayNextButtons();
+        for (JButton button : ingredientButtons){
+            displayNextButtons(button);
+        }
+
     }
 
     private void displayNextButtons(JButton button) {
-        if(counter >ingredientNames.size()){
-
+        if(counter >= ingredientNames.size()){
+            System.out.println("Reached end of ingredient list");
         }
         else{
             button.setText(ingredientNames.get(counter++));
