14fafd17cd7962af634344bf95a5e657f790f7cb
Porslin97 <mimmi.nord97@gmail.com>
2024-04-05 16:56:07 +0200

    Added basic gui with functionallity

--- RecipeInputer/GUI/RecipeInputerMainPanel.java
+++ RecipeInputer/GUI/RecipeInputerMainPanel.java
@@ -3,20 +3,55 @@ package RecipeInputer.GUI;
 import src.GUI.Boundary.MainFrame;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class RecipeInputerMainPanel extends JPanel {
     private RecipeInputerMainFrame mainFrame;
-    private JCheckBox checkBox;
-    private  JTextField recipeNameTextField;
+    private JTextField recipeNameTextField;
+    private ArrayList<JTextField> ingredientNameTextFields = new ArrayList<>();
+    private ArrayList<JCheckBox> alcoholicIngredientCheckBox = new ArrayList<>();
 
     public RecipeInputerMainPanel(RecipeInputerMainFrame mainFrame, int width, int height) {
         this.mainFrame = mainFrame;
+        setLayout(new GridLayout());
         setSize(width, height);
-        checkBox = new JCheckBox("Alcoholic?");
-        add(checkBox);
         recipeNameTextField = new JTextField("Recipe Name:");
         add(recipeNameTextField);
-        JTextField textField = new JTextField();
-        add(textField);
+        for (int i = 0; i < 4; i++) {
+            ingredientNameTextFields.add(new JTextField("Ingredient Name"));
+            add(ingredientNameTextFields.get(i));
+            alcoholicIngredientCheckBox.add(new JCheckBox("Alcoholic?"));
+            add(alcoholicIngredientCheckBox.get(i));
+        }
+        ButtonPanel buttonPanel = new ButtonPanel(this);
+        add(buttonPanel);
+    }
+
+    public void addRecipeToDatabase() {
+        String name;
+        HashMap<String, Boolean> ingredients = new HashMap<>();
+        name = recipeNameTextField.getText();
+        for (int i = 0; i < ingredientNameTextFields.size(); i++){
+            ingredients.put(ingredientNameTextFields.get(i).getText(), alcoholicIngredientCheckBox.get(i).isSelected());
+        }
+        mainFrame.addRecipeToDatabase(name, ingredients);
+    }
+}
+
+class ButtonPanel extends JPanel {
+    private RecipeInputerMainPanel mainPanel;
+    private JButton addRecipeButton;
+
+    public ButtonPanel(RecipeInputerMainPanel mainPanel) {
+        this.mainPanel = mainPanel;
+        addRecipeButton = new JButton("Add recipe");
+        add(addRecipeButton);
+        addRecipeButton.addActionListener(l -> addRecipeToDatabase());
+    }
+
+    private void addRecipeToDatabase() {
+        mainPanel.addRecipeToDatabase();
     }
 }
---
+++ RecipeInputer/RecipeInputerController.java
@@ -0,0 +1,62 @@
+package RecipeInputer;
+
+import RecipeInputer.GUI.RecipeInputerMainFrame;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Savepoint;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class RecipeInputerController {
+    private Connection connection;
+
+    public RecipeInputerController(Connection connection) {
+        this.connection = connection;
+        new RecipeInputerMainFrame(700, 500, this);
+    }
+
+    public void addRecipe(String recipeName, HashMap<String, Boolean> ingredients) {
+        Savepoint savepoint = null;
+        try {
+            savepoint = connection.setSavepoint();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        String sql = "INSERT INTO recipes values (?)";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, recipeName);
+            int rowsAffected = statement.executeUpdate();
+            if (rowsAffected != 1) {
+                System.out.println("Error: Failed to insert recipe");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        String sql2 = "INSERT INTO ingredients values (?, ?)";
+        try (PreparedStatement statement = connection.prepareStatement(sql2)) {
+            // Loop through the array and insert each element individually
+            for (String ingredient : ingredients.keySet()) {
+                // Set the array element as a parameter in the prepared statement
+                statement.setString(1, ingredient);
+                statement.setBoolean(2, ingredients.get(ingredient));
+
+                // Execute the SQL INSERT statement for each array element
+                int rowsAffected = statement.executeUpdate();
+                if (rowsAffected != 1) {
+                    System.err.println("Error: Failed to insert ingredient");
+                    // Rollback to the savepoint
+                    connection.rollback(savepoint);
+                }
+            }
+
+            // Commit the transaction if all inserts were successful
+            connection.commit();
+            System.out.println("Array elements inserted successfully.");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
