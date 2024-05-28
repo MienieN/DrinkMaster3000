4846dc1c8d63e0017c04bc02e99b66d0b9d9b398
Danne1987 <164018827+danne1987@users.noreply.github.com>
2024-05-28 17:23:52 +0200

    Updated the helpscreen for non-alcoholic beverages

--- src/Client/boundary/InstructionScreenManager.java
+++ src/Client/boundary/InstructionScreenManager.java
@@ -30,12 +30,11 @@ public class InstructionScreenManager {
     public void initialize() {
         helpLabel.setMaxWidth(180);
         helpLabel.setWrapText(true);
-        helpLabel.setText("Welcome to the help screen for the alcoholic" +
-                " beverages generator. With this you can find drink" +
-                " recipes based on the ingredients you enter. Use the" +
-                " buttons down below to navigate through the tutorial for" +
-                " this function, or use the buttons above to get the tutorial" +
-                " for the other functions!");
+        helpLabel.setText("Welcome to the help screen! Use" +
+                " the buttons down below to navigate the" +
+                " tutorial for this particular function, or" +
+                " use the buttons above to see the tutorial" +
+                " for any chosen function!");
     }
 
     public void openHelpWindowAlc() {
@@ -118,5 +117,77 @@ public class InstructionScreenManager {
                 " picture to remove the last added ingredient," +
                 " or you can select an ingredient from the marked list" +
                 " and then press the Remove button.");
+    }
+
+    public void openHelpWindowNonAlc() {
+        try {
+            helpStage = new Stage();
+            root = FXMLLoader.load(getClass().getClassLoader().getResource(
+                    "src/Client/resources/fxml/HelpScreenNonAlc.fxml"));
+            scene = new Scene(root);
+            helpStage.setScene(scene);
+            helpStage.setAlwaysOnTop(true);
+            helpStage.setResizable(false);
+            helpStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void displayIntroNonAlc() {
+        helpImage = new Image(getClass().getClassLoader().getResourceAsStream(
+                "src/Client/resources/helppictures/IntroductionNonAlc.png"));
+        helpImageViewer.setImage(helpImage);
+        helpLabel.setText("Welcome to the help screen for the non-alcoholic" +
+                " beverages generator. With this you can find drink" +
+                " recipes based on the ingredients you enter. Use the" +
+                " buttons down below to navigate through the tutorial for" +
+                " this function, or use the buttons above to get the tutorial" +
+                " for the other functions!");
+    }
+
+    public void displayIngredientHelpNonAlc() {
+        helpImage = new Image(getClass().getClassLoader().getResourceAsStream(
+                "src/Client/resources/helppictures/ChooseIngredientNonAlc.png"));
+        helpImageViewer.setImage(helpImage);
+        helpLabel.setText("ingredients will " +
+                "show up at the 4 middle buttons " +
+                "marked by in the window. Here you can " +
+                "choose between the options presented " +
+                "based on what you have at home." +
+                " If you dont have any of the " +
+                "presented ingredients, you can " +
+                "press the None of the above " +
+                "button in the smaller marked " +
+                "area. Pressing the None of the " +
+                "above button will present you " +
+                "with new options to choose from.");
+    }
+
+    public void displayUndoHelpNonAlc() {
+        helpImage = new Image(getClass().getClassLoader().getResourceAsStream(
+                "src/Client/resources/helppictures/RemoveIngredientsNonAlc.png"));
+        helpImageViewer.setImage(helpImage);
+        helpLabel.setText("If you wish to remove an ingredient you" +
+                " have added from the list of added ingredients, you" +
+                " can either press the Undo button as marked in the" +
+                " picture to remove the last added ingredient," +
+                " or you can select an ingredient from the marked list" +
+                " and then press the Remove button.");
+    }
+
+    public void displayChooseDrinkHelpNonAlc() {
+        helpImage = new Image(getClass().getClassLoader().getResourceAsStream(
+                "src/Client/resources/helppictures/ChooseRecipeNonAlc.png"));
+        helpImageViewer.setImage(helpImage);
+        helpLabel.setText("After you have chosen some ingredients, " +
+                "the marked list on the right side " +
+                "will start to show you recipe names " +
+                "based on the ingredients you have " +
+                "chosen. To view the recipe for a drink," +
+                " select it in the menu and click the" +
+                " View Recipe button. This will open a" +
+                " new window with the recipe and instructions.");
     }
 }
