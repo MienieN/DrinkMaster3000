fe0f1da44b26a1a385e1469df87f14d1fdb2347e
Sarah <sarahkjaerulffnehrkorn@gmail.com>
2024-08-03 18:43:59 +0200

    javadoc AdminMain

--- src/admin/AdminMain.java
+++ src/admin/AdminMain.java
@@ -56,7 +56,6 @@ public class AdminMain extends Application {
      */
     public static void main(String[] args) {
         connect();
-        //adminController = new AdminController(connection);
         adminController = new AdminController();
         adminScreen = new AdminScreenManager();
         // Launch the JavaFX application
