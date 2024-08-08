aecdc1165977a9a730edfc7b6f31bbc1bacef524
Tea8616 <153609928+Tea8616@users.noreply.github.com>
2024-08-08 14:24:21 +0200

    Pushed again cause it didn't work the first time

--- src/Client/controller/UserController.java
+++ src/Client/controller/UserController.java
@@ -2,37 +2,55 @@ package src.client.controller;
 
 import src.client.entity.User;
 
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Class to manage user creation and retrieval.
+ */
 public class UserController {
-    private Map<String, User> users = new HashMap<>();
-    private Connection connection;
-    private DatabaseMetaData DatabaseConnection;
+    private Map<String, User> users = new HashMap<>(); // HashMap to store users
 
+    /**
+     * Create a new user with the given username and password.
+     *
+     * @param username Username of the user.
+     * @param password Password of the user.
+     * @return The created user.
+     * @throws SQLException If the user already exists.
+     */
     public User createUser(String username, String password) throws SQLException {
         if (users.containsKey(username)) {
             throw new IllegalArgumentException("User already exists");
         }
 
-        String sql = "INSERT INTO login (username, password) VALUES (?, ?)";
-        try (Connection connection = DatabaseConnection.getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql)) {
-
-            statement.setString(1, username);
-            statement.setString(2, password);
-            statement.executeUpdate();
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
         User user = new User(username, password);
         users.put(username, user);
         return user;
+    }
+
+    /**
+     * Get the user with the given username to check if the user already exists.
+     *
+     * @param username Username of the user.
+     * @return The user with the given username.
+     */
+    public boolean userExists(String username) {
+        return users.containsKey(username);
+    }
+
+    /**
+     * Get the password of the user with the given username.
+     *
+     * @param username Username of the user.
+     * @return The password of the user.
+     */
+    public String getUserPassword(String username) {
+        User user = users.get(username);
+        if (user == null) {
+            throw new IllegalArgumentException("User does not exist");
+        }
+        return user.getPassword();
     }
 }
--- src/Client/entity/User.java
+++ src/Client/entity/User.java
@@ -1,18 +1,28 @@
 package src.client.entity;
 
+/**
+ * Class to represent a user.
+ */
 public class User {
-    private String username;
-    private String password;
+    private String username; // Username of the user
+    private String password; // Password of the user
 
+    /**
+     * Create a new user with the given username and password.
+     *
+     * @param username Username of the user.
+     * @param password Password of the user.
+     */
     public User(String username, String password) {
         this.username = username;
         this.password = password;
     }
 
-    public String getUsername() {
-        return username;
-    }
-
+    /**
+     * Get the password of the user.
+     *
+     * @return The password of the user.
+     */
     public String getPassword() {
         return password;
     }
